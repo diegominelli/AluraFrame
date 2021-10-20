5ff5e28d040a0c491be88e97ed59e260fbb42269
Diego Minelli <diego.minelli@hotmail.com>
2021-10-20 15:28:34 -0300

    Associando metodos do controller as acoes do usuario

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,6 +1,13 @@
 class NegociacaoController {
   adiciona(event) {
     event.preventDefault();
-    alert('Chamei ação do controller');
+    let $ = document.querySelector.bind(document); // .bind(document) para o querySelector se referenciar pelo document.
+    let inputData = $('#data');
+    let inputQuantidade = $('#quantidade');
+    let inputValor = $('#valor');
+
+    console.log(inputData.value);
+    console.log(inputQuantidade.value);
+    console.log(inputValor.value);
   }
 }
