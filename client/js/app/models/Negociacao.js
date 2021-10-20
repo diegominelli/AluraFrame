f1577fa0acf4affa2e29c885f35f1e860130110d
Diego Minelli <diego.minelli@hotmail.com>
2021-10-20 12:12:15 -0300

    Fazendo o encapsulamento das propriedades da classe

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,11 +1,23 @@
 class Negociacao {
   constructor(data, quantidade, valor) {
-    this.data = data;
-    this.quantidade = quantidade;
-    this.valor = valor;
+    this._data = data;
+    this._quantidade = quantidade;
+    this._valor = valor;
   }
 
-  obtemVolume() {
-    return this.quantidade * this.valor;
+  getVolume() {
+    return this._quantidade * this._valor; // _ significa que a propriedade de uma classe não podem ser modificadas.
+  }
+
+  getData() {
+    return this._data;
+  }
+
+  getQuantidade() {
+    return this._quantidade;
+  }
+
+  getValor() {
+    return this._valor;
   }
 }
