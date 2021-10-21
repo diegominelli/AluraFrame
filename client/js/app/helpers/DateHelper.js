ff4e10da6303fb244cb367351b20c7e7e12f21ba
Diego Minelli <diego.minelli@hotmail.com>
2021-10-21 11:22:57 -0300

    Isolando a responsabilidade de conversao de datas

---
+++ client/js/app/helpers/DateHelper.js
@@ -0,0 +1,13 @@
+class DataHelper {
+  dataParaTexto(data) {
+    return (
+      data.getDate() + '/' + (data.getMonth() + 1) + '/' + data.getFullYear()
+    );
+  }
+
+  textoParaData(texto) {
+    return new Date(
+      ...texto.split('-').map((item, indice) => item - (indice % 2))
+    );
+  }
+}
