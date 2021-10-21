952a53be95796c774b8dca0733dd5be02fd5ad40
Diego Minelli <diego.minelli@hotmail.com>
2021-10-21 17:00:43 -0300

    Totalizando o volume em nosso template com a funcao reduce

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -16,19 +16,25 @@ class NegociacoesView {
 
         <tbody>
             ${model.negociacoes
-              .map((n) => {
-                return `
-                <tr>
-                    <td>${DateHelper.dataParaTexto(n.data)}</td>
-                    <td>${n.quantidade}</td>
-                    <td>${n.valor}</td>
-                    <td>${n.volume}</td>
-                </tr>
-            `;
-              })
+              .map(
+                (n) =>
+                  `
+                    <tr>
+                        <td>${DateHelper.dataParaTexto(n.data)}</td>
+                        <td>${n.quantidade}</td>
+                        <td>${n.valor}</td>
+                        <td>${n.volume}</td>
+                    </tr>
+                `
+              )
               .join('')}
         </tbody>
         <tfoot>
+        <td colspan="3"></td>
+        <td>${model.negociacoes.reduce(
+          (total, n) => total + n.volume,
+          0.0
+        )}</td>
         </tfoot>
     </table>   
     `;
