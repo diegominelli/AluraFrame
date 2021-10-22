b437bc95ca2fa7a13604e7c05a4965655f2b69e0
Diego Minelli <diego.minelli@hotmail.com>
2021-10-22 14:42:58 -0300

    Usando o Reflect apply para alterar o contexto do this

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -4,7 +4,9 @@ class NegociacaoController {
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
-    this._listaNegociacoes = new ListaNegociacoes();
+    this._listaNegociacoes = new ListaNegociacoes(this, function (model) {
+      this._negociacoesView.update(model);
+    });
 
     this._negociacoesView = new NegociacoesView($('#negociacoesView'));
     this._negociacoesView.update(this._listaNegociacoes);
@@ -13,10 +15,10 @@ class NegociacaoController {
     this._mensagemView = new MensagemView($('#mensagemView'));
     this._mensagemView.update(this._mensagem);
   }
+
   adiciona(event) {
     event.preventDefault();
     this._listaNegociacoes.adiciona(this._criaNegociacao());
-    this._negociacoesView.update(this._listaNegociacoes);
 
     this._mensagem.texto = 'Negociação adicionada com secesso';
     this._mensagemView.update(this._mensagem);
@@ -26,7 +28,6 @@ class NegociacaoController {
 
   apaga() {
     this._listaNegociacoes.esvazia();
-    this._negociacoesView.update(this._listaNegociacoes);
 
     this._mensagem.texto = 'Negociações apagadas com sucesso';
     this._mensagemView.update(this._mensagem);
