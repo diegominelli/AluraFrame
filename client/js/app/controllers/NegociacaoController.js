959d1c07a295a38c6848c63592c5b89df23490cd
Diego Minelli <diego.minelli@hotmail.com>
2021-10-26 17:06:40 -0300

    Removendo as negociacoes

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -23,9 +23,12 @@ class NegociacaoController {
       .then((connection) => new NegociacaoDao(connection))
       .then((dao) => dao.listaTodos())
       .then((negociacoes) =>
-        negociacoes.forEach((negociacao) =>
-          this._listaNegociacoes.adiciona(negociacao)
-        )
+        negociacoes
+          .forEach((negociacao) => this._listaNegociacoes.adiciona(negociacao))
+          .catch((erro) => {
+            console.log(erro);
+            this._mensagem.texto = error;
+          })
       );
   }
 
@@ -62,8 +65,13 @@ class NegociacaoController {
   }
 
   apaga() {
-    this._listaNegociacoes.esvazia();
-    this._mensagem.texto = 'Negociações apagadas com sucesso';
+    ConnectionFactory.getConnection()
+      .then((connection) => new NegociacaoDao(connection))
+      .then((dao) => dao.apagaTodos())
+      .then((mensagem) => {
+        this._mensagem.texto = mensagem;
+        this._listaNegociacoes.esvazia();
+      });
   }
 
   _criaNegociacao() {
